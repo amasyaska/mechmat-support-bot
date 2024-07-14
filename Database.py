e30c2f48f4e9498cd92870930a8dfb18f25757b0
amasyaska <155889990+amasyaska@users.noreply.github.com>
2024-07-14 18:05:07 +0300

    added Database.get_delivery_chat_id_list()

--- Database.py
+++ Database.py
@@ -7,7 +7,7 @@ class Database:
     """
     def __init__(self: Database, filename: str="database.db") -> None:
         self.db_filename = filename
-        self.db_connection = sqlite3.connect(self.db_filename)
+        self.db_connection = sqlite3.connect(self.db_filename, check_same_thread=False)
         self.db_cursor = self.db_connection.cursor()
         # creating tables in database if they are not existent
         self.db_cursor.execute(f'''CREATE TABLE IF NOT EXISTS User(
@@ -16,7 +16,7 @@ class Database:
                                );''')
         self.db_connection.commit()
 
-    def subscribe_user_delivery_by_chat_id(self: Database, chat_id) -> None:
+    def subscribe_user_delivery_by_chat_id(self: Database, chat_id: int) -> None:
         self.db_cursor.execute(f'''INSERT INTO User(chat_id, delivery)
                                     VALUES
                                     ({chat_id}, TRUE)
@@ -25,7 +25,7 @@ class Database:
                                      WHERE chat_id={chat_id};''')
         self.db_connection.commit()
 
-    def unsubscribe_user_delivery_by_chat_id(self: Database, chat_id) -> None:
+    def unsubscribe_user_delivery_by_chat_id(self: Database, chat_id: int) -> None:
         self.db_cursor.execute(f'''INSERT INTO User(chat_id, delivery)
                                     VALUES
                                     ({chat_id}, FALSE)
@@ -34,6 +34,10 @@ class Database:
                                      WHERE chat_id={chat_id};''')
         self.db_connection.commit()
 
+    def get_delivery_chat_id_list(self: Database) -> list[int]:
+        return [elem[0] for elem in self.db_cursor.execute(f'''SELECT chat_id
+                                    FROM User;
+                               ''').fetchall()] # using list comprehension to form list of values instead of list of tuples
+
 if __name__ == "__main__":
     db = Database()
-    db.unsubscribe_user_delivery_by_chat_id(12)
