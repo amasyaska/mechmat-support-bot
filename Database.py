9c9555c547d87cb84214cd486fa9a860afced4ec
amasyaska <155889990+amasyaska@users.noreply.github.com>
2024-07-14 19:56:55 +0300

    added methods for authorization

--- Database.py
+++ Database.py
@@ -14,6 +14,10 @@ class Database:
                                     chat_id INT PRIMARY KEY,
                                     delivery INT
                                );''')
+        self.db_cursor.execute(f'''CREATE TABLE IF NOT EXISTS Admin(
+                                    chat_id INT PRIMARY KEY,
+                                    is_delivering INT
+                               );''')
         self.db_connection.commit()
 
     def subscribe_user_delivery_by_chat_id(self: Database, chat_id: int) -> None:
@@ -38,6 +42,37 @@ class Database:
         return [elem[0] for elem in self.db_cursor.execute(f'''SELECT chat_id
                                     FROM User;
                                ''').fetchall()] # using list comprehension to form list of values instead of list of tuples
+    
+    def add_admin_by_chat_id(self: Database, chat_id: int) -> None:
+        self.db_cursor.execute(f'''INSERT INTO Admin(chat_id, is_delivering)
+                                    VALUES
+                                    ({chat_id}, FALSE)
+                                    ON CONFLICT(chat_id) DO UPDATE
+                                     SET is_delivering=FALSE
+                                     WHERE chat_id={chat_id};''')
+        self.db_connection.commit()
+    
+    def set_admin_to_delivering_by_chat_id(self: Database, chat_id: int) -> None:
+        self.db_cursor.execute(f'''UPDATE Admin
+                                    SET is_delivering=TRUE
+                                    WHERE chat_id={chat_id}''')
+        self.db_connection.commit()
+
+    def unset_admin_to_delivering_by_chat_id(self: Database, chat_id: int) -> None:
+        self.db_cursor.execute(f'''UPDATE Admin
+                                    SET is_delivering=FALSE
+                                    WHERE chat_id={chat_id}''')
+        self.db_connection.commit()
+
+    def is_admin_delivering_by_chat_id(self: Database, chat_id: int) -> bool:
+        return bool(self.db_cursor.execute(f'''SELECT is_delivering
+                                                FROM Admin
+                                                WHERE chat_id={chat_id}''').fetchone()[0])
+    
+    def is_authorized_chat_id(self: Database, chat_id: int) -> bool:
+        return (chat_id in [elem[0] for elem in self.db_cursor.execute(f'''SELECT chat_id
+                                    FROM Admin;
+                               ''').fetchall()]) # using list comprehension to form list of values instead of list of tuples
 
 if __name__ == "__main__":
     db = Database()
