881a2adb36b66d631eb5bb023d26da33fa423517
amasyaska <155889990+amasyaska@users.noreply.github.com>
2024-07-25 14:13:24 +0300

    reimplemented a bunch of Database methods to work with User and Admin tables

--- Database.py
+++ Database.py
@@ -12,67 +12,122 @@ class Database:
         # creating tables in database if they are not existent
         self.db_cursor.execute(f'''CREATE TABLE IF NOT EXISTS User(
                                     chat_id INT PRIMARY KEY,
+                                    state INT,
                                     delivery INT
                                );''')
         self.db_cursor.execute(f'''CREATE TABLE IF NOT EXISTS Admin(
                                     chat_id INT PRIMARY KEY,
-                                    is_delivering INT
+                                    state INT,
+                                    is_superadmin INT
+                               );''')
+        self.db_cursor.execute(f'''CREATE TABLE IF NOT EXISTS OneTimePassword(
+                                    admin_chat_id INT,
+                                    password CHAR
                                );''')
         self.db_connection.commit()
 
-    def subscribe_user_delivery_by_chat_id(self: Database, chat_id: int) -> None:
-        self.db_cursor.execute(f'''INSERT INTO User(chat_id, delivery)
+    # USER
+
+    def set_user_delivery_by_chat_id(self: Database, chat_id: int, delivery: int) -> None:
+        self.db_cursor.execute(f'''UPDATE User
+                                    SET delivery={delivery}
+                                    WHERE chat_id={chat_id}''')
+        self.db_connection.commit()
+
+    def add_user_by_chat_id(self: Database, chat_id: int) -> None:
+        self.db_cursor.execute(f'''INSERT INTO User(chat_id, state, delivery)
                                     VALUES
-                                    ({chat_id}, TRUE)
+                                    ({chat_id}, 0, FALSE)
                                     ON CONFLICT(chat_id) DO UPDATE
-                                     SET delivery=TRUE
+                                     SET state=0, delivery=FALSE
                                      WHERE chat_id={chat_id};''')
         self.db_connection.commit()
 
-    def unsubscribe_user_delivery_by_chat_id(self: Database, chat_id: int) -> None:
-        self.db_cursor.execute(f'''INSERT INTO User(chat_id, delivery)
-                                    VALUES
-                                    ({chat_id}, FALSE)
-                                    ON CONFLICT(chat_id) DO UPDATE
-                                     SET delivery=FALSE
-                                     WHERE chat_id={chat_id};''')
+    def is_user_exists_by_chat_id(self: Database, chat_id: int) -> bool:
+        return (len(self.db_cursor.execute(f'''SELECT *
+                                            FROM User
+                                            WHERE chat_id={chat_id};
+                                            ''').fetchall()) > 0)
+
+    def set_user_state_by_chat_id(self: Database, chat_id: int, state: int) -> None:
+        self.db_cursor.execute(f'''UPDATE User
+                                    SET state={state}
+                                    WHERE chat_id={chat_id}''')
         self.db_connection.commit()
+
+    def get_user_state_by_chat_id(self: Database, chat_id: int) -> int:
+        return self.db_cursor.execute(f'''SELECT state
+                                                FROM User
+                                                WHERE chat_id={chat_id}''').fetchone()[0]
+
+    def get_user_delivery_by_chat_id(self: Database, chat_id: int) -> int:
+        return self.db_cursor.execute(f'''SELECT delivery
+                                                FROM User
+                                                WHERE chat_id={chat_id}''').fetchone()[0]
 
     def get_delivery_chat_id_list(self: Database) -> list[int]:
+        # better use iterator
         return [elem[0] for elem in self.db_cursor.execute(f'''SELECT chat_id
                                     FROM User;
                                ''').fetchall()] # using list comprehension to form list of values instead of list of tuples
     
+    # ADMINISTRATION
+    
     def add_admin_by_chat_id(self: Database, chat_id: int) -> None:
-        self.db_cursor.execute(f'''INSERT INTO Admin(chat_id, is_delivering)
+        self.db_cursor.execute(f'''INSERT INTO Admin(chat_id, state, is_superadmin)
                                     VALUES
-                                    ({chat_id}, FALSE)
+                                    ({chat_id}, 0, 0)
                                     ON CONFLICT(chat_id) DO UPDATE
-                                     SET is_delivering=FALSE
+                                     SET state=0, is_superadmin=0
                                      WHERE chat_id={chat_id};''')
         self.db_connection.commit()
     
-    def set_admin_to_delivering_by_chat_id(self: Database, chat_id: int) -> None:
+    def set_admin_state_by_chat_id(self: Database, chat_id: int, state: int) -> None:
         self.db_cursor.execute(f'''UPDATE Admin
-                                    SET is_delivering=TRUE
+                                    SET state={state}
                                     WHERE chat_id={chat_id}''')
         self.db_connection.commit()
 
-    def unset_admin_to_delivering_by_chat_id(self: Database, chat_id: int) -> None:
+    def set_superadmin_by_chat_id(self: Database, chat_id: int, is_superadmin: int) -> None:
         self.db_cursor.execute(f'''UPDATE Admin
-                                    SET is_delivering=FALSE
+                                    SET is_superadmin={is_superadmin}
                                     WHERE chat_id={chat_id}''')
         self.db_connection.commit()
 
-    def is_admin_delivering_by_chat_id(self: Database, chat_id: int) -> bool:
-        return bool(self.db_cursor.execute(f'''SELECT is_delivering
+    def get_admin_state_by_chat_id(self: Database, chat_id: int) -> int:
+        return self.db_cursor.execute(f'''SELECT state
                                                 FROM Admin
-                                                WHERE chat_id={chat_id}''').fetchone()[0])
+                                                WHERE chat_id={chat_id}''').fetchone()[0]
     
-    def is_authorized_chat_id(self: Database, chat_id: int) -> bool:
-        return (chat_id in [elem[0] for elem in self.db_cursor.execute(f'''SELECT chat_id
-                                    FROM Admin;
-                               ''').fetchall()]) # using list comprehension to form list of values instead of list of tuples
+    def is_admin_by_chat_id(self: Database, chat_id: int) -> bool:
+        return (len(self.db_cursor.execute(f'''SELECT *
+                                            FROM Admin
+                                            WHERE chat_id={chat_id};
+                                            ''').fetchall()) > 0)
+    
+    def is_superadmin_by_chat_id(self: Database, chat_id: int) -> bool:
+        return (len(self.db_cursor.execute(f'''SELECT *
+                                            FROM Admin
+                                            WHERE chat_id={chat_id} AND is_superadmin=1;
+                                            ''').fetchall()) > 0)
+    
+    def create_one_time_password(self: Database, chat_id: int, password: str) -> None:
+        self.db_cursor.execute(f'''INSERT INTO OneTimePassword(admin_chat_id, password)
+                                    VALUES
+                                    ({chat_id}, "{password}");''')
+        self.db_connection.commit()
+
+    def delete_one_time_password(self: Database, password: str) -> None:
+        self.db_cursor.execute(f'''DELETE FROM OneTimePassword
+                                    WHERE
+                                    password="{password}";''')
+        self.db_connection.commit()
+
+    def is_one_time_password(self: Database, password: str) -> bool:
+        return (len(self.db_cursor.execute(f'''SELECT *
+                                            FROM OneTimePassword
+                                            WHERE password="{password}";
+                                            ''').fetchall()) > 0)
 
 if __name__ == "__main__":
     db = Database()
